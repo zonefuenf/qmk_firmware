49d64479a61a77adec1f346453c64aaaf3ea25c4
zonefuenf <github@philipp.philippleser.de>
2019-07-13 22:00:54 +0200

    remove single tap behavior for shift keys

--- users/zonefuenf/zonefuenf.c
+++ users/zonefuenf/zonefuenf.c
@@ -128,7 +128,7 @@ static xtap rshift_tap_state = {
 void lshift_finished (qk_tap_dance_state_t *state, void *user_data) {
   lshift_tap_state.state = hold_cur_dance(state);
   switch (lshift_tap_state.state) {
-    case SINGLE_TAP: register_shift_eight(); break;
+    case SINGLE_TAP: register_code(KC_LSFT); break;
     case SINGLE_HOLD: register_code(KC_LSFT); break;
     case DOUBLE_TAP: register_altgr_seven(); break;
     case TRIPLE_TAP: register_altgr_eight(); break;
@@ -137,7 +137,7 @@ void lshift_finished (qk_tap_dance_state_t *state, void *user_data) {
 
 void lshift_reset (qk_tap_dance_state_t *state, void *user_data) {
   switch (lshift_tap_state.state) {
-    case SINGLE_TAP: unregister_shift_eight(); break;
+    case SINGLE_TAP: unregister_code(KC_LSFT); break;
     case SINGLE_HOLD: unregister_code(KC_LSFT); break;
     case DOUBLE_TAP: unregister_altgr_seven(); break;
     case TRIPLE_TAP: unregister_altgr_eight();
@@ -148,7 +148,7 @@ void lshift_reset (qk_tap_dance_state_t *state, void *user_data) {
 void rshift_finished (qk_tap_dance_state_t *state, void *user_data) {
   rshift_tap_state.state = hold_cur_dance(state);
   switch (rshift_tap_state.state) {
-    case SINGLE_TAP: register_shift_nine(); break;
+    case SINGLE_TAP: register_code(KC_RSFT); break;
     case SINGLE_HOLD: register_code(KC_RSFT); break;
     case DOUBLE_TAP: register_altgr_zero(); break;
     case TRIPLE_TAP: register_altgr_nine(); break;
@@ -157,7 +157,7 @@ void rshift_finished (qk_tap_dance_state_t *state, void *user_data) {
 
 void rshift_reset (qk_tap_dance_state_t *state, void *user_data) {
   switch (rshift_tap_state.state) {
-    case SINGLE_TAP: unregister_shift_nine(); break;
+    case SINGLE_TAP: unregister_code(KC_RSFT); break;
     case SINGLE_HOLD: unregister_code(KC_RSFT); break;
     case DOUBLE_TAP: unregister_altgr_zero(); break;
     case TRIPLE_TAP: unregister_altgr_nine();
