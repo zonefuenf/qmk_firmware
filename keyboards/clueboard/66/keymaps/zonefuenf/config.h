2ca5e6d707505bb105661d192b49ea87b0a9adf1
zonefuenf <github@philipp.philippleser.de>
2019-11-09 19:22:51 +0100

    Simplify tapping behavior

--- keyboards/clueboard/66/keymaps/zonefuenf/config.h
+++ keyboards/clueboard/66/keymaps/zonefuenf/config.h
@@ -71,5 +71,5 @@
 #define RGBLIGHT_EFFECT_KNIGHT_LED_NUM 5  // How many LEDs to travel
 #define RGBLIGHT_EFFECT_SNAKE_LENGTH 4    // How many LEDs wide to light up
 
-#define TAPPING_TERM 300
+#define TAPPING_TERM 400
 
--- users/zonefuenf/zonefuenf.c
+++ users/zonefuenf/zonefuenf.c
@@ -7,50 +7,28 @@
 
 int cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
-    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
-    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
-    else return SINGLE_HOLD;
+    return SINGLE_HOLD;
   }
   else if (state->count == 2) {
-    /*
-     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-     * keystrokes of the key, and not the 'double tap' action/macro.
-    */
-    if (state->interrupted) return DOUBLE_SINGLE_TAP;
-    else if (state->pressed) return DOUBLE_HOLD;
-    else return DOUBLE_TAP;
+    return DOUBLE_TAP;
   }
-  //Assumes no one is trying to type the same letter three times (at least not quickly).
-  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
-  if (state->count == 3) {
-    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
-    else return TRIPLE_HOLD;
+  else if (state->count == 3) {
+   return TRIPLE_TAP;
   }
   else return 8; //magic number. At some point this method will expand to work for more presses
 }
 
 int hold_cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
-    if (state->interrupted) {
-      if (!state->pressed) return SINGLE_TAP;
-      else return SINGLE_HOLD;
-    }
-    else {
-      if (!state->pressed) return SINGLE_TAP;
-      else return SINGLE_HOLD;
-    }
+ return SINGLE_HOLD;
   }
   //If count = 2, and it has been interrupted - assume that user is trying to type the letter associated
   //with single tap.
   else if (state->count == 2) {
-    if (state->pressed) return DOUBLE_HOLD;
-    else return DOUBLE_TAP;
+    return DOUBLE_TAP;
   }
   else if (state->count == 3) {
-    if (!state->pressed) return TRIPLE_TAP;
-    else return TRIPLE_HOLD;
+    return TRIPLE_TAP;
   }
   else return 8; //magic number. At some point this method will expand to work for more presses
 }
@@ -128,7 +106,6 @@ static xtap rshift_tap_state = {
 void lshift_finished (qk_tap_dance_state_t *state, void *user_data) {
   lshift_tap_state.state = hold_cur_dance(state);
   switch (lshift_tap_state.state) {
-    case SINGLE_TAP: register_code(KC_LSFT); break;
     case SINGLE_HOLD: register_code(KC_LSFT); break;
     case DOUBLE_TAP: register_altgr_seven(); break;
     case TRIPLE_TAP: register_altgr_eight(); break;
@@ -137,7 +114,6 @@ void lshift_finished (qk_tap_dance_state_t *state, void *user_data) {
 
 void lshift_reset (qk_tap_dance_state_t *state, void *user_data) {
   switch (lshift_tap_state.state) {
-    case SINGLE_TAP: unregister_code(KC_LSFT); break;
     case SINGLE_HOLD: unregister_code(KC_LSFT); break;
     case DOUBLE_TAP: unregister_altgr_seven(); break;
     case TRIPLE_TAP: unregister_altgr_eight();
@@ -148,7 +124,6 @@ void lshift_reset (qk_tap_dance_state_t *state, void *user_data) {
 void rshift_finished (qk_tap_dance_state_t *state, void *user_data) {
   rshift_tap_state.state = hold_cur_dance(state);
   switch (rshift_tap_state.state) {
-    case SINGLE_TAP: register_code(KC_RSFT); break;
     case SINGLE_HOLD: register_code(KC_RSFT); break;
     case DOUBLE_TAP: register_altgr_zero(); break;
     case TRIPLE_TAP: register_altgr_nine(); break;
@@ -157,7 +132,6 @@ void rshift_finished (qk_tap_dance_state_t *state, void *user_data) {
 
 void rshift_reset (qk_tap_dance_state_t *state, void *user_data) {
   switch (rshift_tap_state.state) {
-    case SINGLE_TAP: unregister_code(KC_RSFT); break;
     case SINGLE_HOLD: unregister_code(KC_RSFT); break;
     case DOUBLE_TAP: unregister_altgr_zero(); break;
     case TRIPLE_TAP: unregister_altgr_nine();
